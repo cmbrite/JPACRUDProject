2dd6ee94f95526d0be6bd1c11302b3f46a85996b
Chris Brite <chrismbrite@gmail.com>
2022-05-13 15:50:46 -0700

    Create Boot MVC project

---
+++ DogParkApp/src/main/java/com/skilldistillery/dogparks/DogParkAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dogparks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogParkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogParkAppApplication.class, args);
+	}
+
+}
